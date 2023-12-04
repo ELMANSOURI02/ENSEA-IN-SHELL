e51ba6c4e6c2f41e41c8190e3aea7243861f42c6
ELMANSOURI02 <nada.elmansouri@ensea.fr>
2023-12-04 09:39:25 +0100

    question 5

--- ENSEA.c
+++ ENSEA.c
@@ -1,66 +1,109 @@
-#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
 
 #define MAX_INPUT_SIZE 1024
 
+// Function to print a welcome message in the shell
 void printWelcomeMessage() {
-    write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\n", 30);
-    write(STDOUT_FILENO, "Pour quitter, tapez 'exit'.\n", 28);
+    write(STDOUT_FILENO, "Welcome to the ENSEA Shell.\n", 29);
+    write(STDOUT_FILENO, "To exit, type 'exit'.\n", 22);
 }
 
-void printPrompt(int exitCode, int signalCode) {
+// Function to print the shell prompt, including exit code, signal code, and elapsed time
+void printPrompt(int exitCode, int signalCode, long elapsedTime) {
     if (WIFEXITED(exitCode)) {
-        printf("enseash [exit:%d] %% ", WEXITSTATUS(exitCode));
+        char exitMsg[50];
+        // Format the exit message
+        sprintf(exitMsg, "enseash [exit:%d|%ldms] %% ", WEXITSTATUS(exitCode), elapsedTime);
+        // Write the exit message to STDOUT
+        write(STDOUT_FILENO, exitMsg, strlen(exitMsg));
     } else if (WIFSIGNALED(exitCode)) {
-        printf("enseash [sign:%d] %% ", WTERMSIG(exitCode));
+        char signMsg[50];
+        // Format the signal message
+        sprintf(signMsg, "enseash [sign:%d|%ldms] %% ", WTERMSIG(exitCode), elapsedTime);
+        // Write the signal message to STDOUT
+        write(STDOUT_FILENO, signMsg, strlen(signMsg));
     } else {
+        // Default prompt if no exit code or signal
         write(STDOUT_FILENO, "enseash % ", 11);
     }
 }
 
+// Function to measure the execution time of the command and execute the command
+void measureAndExecute(char* command) {
+    struct timespec start, end;
+    long elapsedTime;
+
+    // Record the start time using clock_gettime
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // Fork a child process to execute the command
+    int child_pid = fork();
+
+    if (child_pid == 0) {
+        // Child process: execute the command using execlp
+        execlp(command, command, (char *)NULL);
+        
+        exit(EXIT_FAILURE);
+    } else if (child_pid > 0) {
+        // Parent process
+        int status;
+        waitpid(child_pid, &status, 0);
+
+        // Record the end time using clock_gettime
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        // Calculate the elapsed time in milliseconds
+        elapsedTime = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+      
+        // Print the shell prompt with exit code, signal code, and elapsed time
+        printPrompt(status, 0, elapsedTime);
+    } else {
+        // Error handling for fork failure
+        exit(EXIT_FAILURE);
+    }
+    
+}
+
+// Main function
 int main() {
+    // Print the welcome message at the start
     printWelcomeMessage();
 
     while (1) {
-        int exitCode = 0;
-        int signalCode = 0;
+        
+        printPrompt(0, 0, 0);
 
-        printPrompt(exitCode, signalCode);
-
+        // Read user input
         char input[MAX_INPUT_SIZE];
         if (fgets(input, sizeof(input), stdin) == NULL) {
-            // If fgets returns NULL, the user either used <Ctrl>+d or there is an error.
-            write(STDOUT_FILENO, "Bye bye...\n", 11);
+            // Handle end-of-file (Ctrl+D) scenario
+            write(STDOUT_FILENO, "Bye bye (end of file)...\n", 26);
             break;
         }
 
-        input[strcspn(input, "\n")] = '\0';  // delete the new carater of the ligne
+        // Remove the newline character from the input
+        input[strcspn(input, "\n")] = '\0';
 
+        // Check for the exit command
         if (strcmp(input, "exit") == 0) {
+            // Print a goodbye message and exit the loop
             write(STDOUT_FILENO, "Bye bye...\n", 11);
             break;
         }
 
+        // Check if the input is not empty
         if (strlen(input) > 0) {
-            int child_pid = fork();
-
-            if (child_pid == 0) {
-                // enfant Process 
-                execlp(input, input, (char *)NULL);
-                perror("Erreur lors de l'exécution de la commande");
-                exit(EXIT_FAILURE);
-            } else if (child_pid > 0) {
-                // parent Process 
-                waitpid(child_pid, &exitCode, 0);
-            } else {
-                perror("Erreur lors de la création du processus fils");
-                exit(EXIT_FAILURE);
-            }
+            // Measure the time of command execution and execute the command
+            measureAndExecute(input);
         }
     }
+    
 
     return 0;
 }
+
